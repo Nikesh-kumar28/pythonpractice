c9e9a9e9ebc1b313cf4061b44ec38148e5e84f20
lyfofnikesh <ny0062036@gmail.com>
2026-09-06 19:33:41 +0530

    add github learn

---
+++ github.py
@@ -0,0 +1,44 @@
+# Git ka use
+
+
+# Sbse phle ham
+
+# 1. Git init krte h
+
+# 2. Git status
+
+# 3. File add krni h only 1,2 use  git add  .
+
+# 4. If sabi file add krni h tho git add .
+
+# 5. And check status use git status of all files.
+
+# 6. Msg k liye git  commit -m " " Msg likhna hoga.
+
+# 7. Fr hamse puche ga who r u fr hame apni gamil GitHub wali or apna user name github wala btana hoga globally jisse in future dubara btana na pade.
+
+# 8. Git diff.  Jo file add ki h unme diff or changes dekhne k liye or ye commit karne se phle check hota h .
+
+# 9. Git log use hota h Btane m liye kisi folder ki history kya h local folder ki tracking k liye git log use hota h.
+
+# 10. Markdown (md) file k liye ham vs code me use krte h ctrl + shift + v ye kaam aata h html code ko lang. Me change krne me.
+
+# 11. Powershell means terminal kabi kabi output ko pager me khol deta h . Isse bhar aane k liye Q keyword dbana hota h.
+
+# 12. Jab ham kisi file ko lock kar dete h or fr usme change krte h tb vs code uss file k aage M dikha deta h means updated.
+
+# 13. Git hard use hota h pichle changes par wapas jaane k liye git hard use hota h.
+
+# 14. Ipconfig  laptop ip adress and ping Google.com Google ip address .
+
+# 15. Terminal me server start Karo python -m http.server and uske baad apna ip adress daalo or
+#  pichee lgao 8000 uske baad hamara jo bhi folder hoga vo open ho jaaye ga.
+
+# 16. GitHub ki repogrtroty ko download karne k liye ham command use karte h git
+#  clone or GitHub ki repogrtroty ka url us fr link.
+
+# 17. Github se ham kuch bhi clone karte h vo hamesa ek repogrtroty ban k aati h normal folder nhi aata.
+
+# 18. Local repogrtroty means laptop wali and remote means GitHub wali.
+
+# cd — Change Directory.
