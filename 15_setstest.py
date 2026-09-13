c333dd65feb59fc075c974e9e9442dc6bbc7c9c9
lyfofnikesh <ny0062036@gmail.com>
2026-09-13 12:40:17 +0530

    Add files via upload

---
+++ 15_setstest.py
@@ -0,0 +1,47 @@
+# a = {12, 34, 56, 788, 999, 45}
+# print(a)
+
+# b = {23, 45, 34, 34, 23, "345", 345}
+# print(b)
+
+# c = {23, 45, (34, 56, 78), 3, 2345, 23456, "23", "23"}
+# print(c)
+
+# a = {23, True, 1, False, 0}
+# print(a)
+
+# c = {23, 456, True, 1}
+# print(c)
+
+# c = {23, 456, 1, True}
+# print(c)
+
+# empty_set = set()
+# print(empty_set, type(empty_set))
+
+# a = { (), False }
+# print(a)
+# print(a[0])
+
+# b = [ (), True]
+# print(b)
+# print(b[0])
+
+# a = {1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, "Nikku", "nikku", True, 0, 1, False}
+# print(a)
+
+# a = "rajasthan"
+# my_set = set(a)
+# print(my_set)
+
+# a = (1, 1, 2, 2, 5, 6, 7)
+# my_set = set(a)
+# print(my_set)
+
+# a = (2, 2, 2, 3, 4, 4, 5, 6, 7, 7, True, 1, 0, False)
+# my_set = set(a)
+# print(my_set)
+
+# a = {12, 34, 123, (123, 34), "2345", 34.56, False, 0, 1, True}
+# print(a)
+
