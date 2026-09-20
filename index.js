10e0b0956b44ad2af73be4df470faba6498b16fd
lyfofnikesh <ny0062036@gmail.com>
2026-09-20 15:21:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,97 @@
+// const clickhandle=()=> {
+//    document.getElementById("main").innerHTML = 'Hii I Am Here'
+// }
+
+// variable key
+
+// var
+// var a = 23456
+// if (true) {
+//     var a = 5000
+//      console.log(a);
+// }
+// console.log(a);
+        
+// let a = 60 
+// a = 100
+// console.log(a);
+
+// const a = 200
+// console.log(a)
+
+// const a = 590
+
+// console.log(a);
+        
+// if (true) {
+//     let a = 340
+//     console.log(a);
+// }
+
+// console.log(a);
+
+// let a = 40
+
+// if (true) {
+//       a = 100
+//        console.log(a);
+// }
+// console.log(a);
+
+// let $a = 50
+// console.log($a);
+
+// let a = 5084
+// console.log(a);
+
+// premitive data type
+// b  bigInt
+// b  boolean
+// n  null  20   2345678
+// n  number 1234.345678
+// s  symbole("r4rfg6ygtrfvc")
+// s  string  
+// u   undefined
+
+// non-premitive datatype
+
+// let arr = ["jai", "vijay", "ronak"]
+// console.log(arr[arr.length -1]); 
+
+// let arr = ["jai", "vijay", "ronak"]
+// console.log(arr[2]);
+
+// object
+
+// let obj = {
+//    name: "jai",
+//    age: 40,
+//    city: "jaipur"
+// }
+// console.log(obj);
+// console.log(obj.name);
+// console.log(obj.age);
+// console.log(obj.city);
+
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+
+
+// name = "yadav"
+// a = `jai ${name}`
+// console.log(a);
+
+// var a 
+// console.log(a);
+  
+// let a = Symbol ("3edft67uhgfr")
+// console.log(typeof (a));
+
+
+// Function
+// function sum() {
+//      return 20 + 30
+// }
+
+// console.log(sum());
+
