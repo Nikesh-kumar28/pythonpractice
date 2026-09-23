ffe7f042a77bd447c5ccb0627926780d0f84ac06
lyfofnikesh <ny0062036@gmail.com>
2026-09-23 13:54:30 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -95,3 +95,177 @@
 
 // console.log(sum());
 
+// function Expression
+
+// let func = function sum(s, n) {
+//     return s + n
+// }
+
+// console.log(sum(20, 40) + sum(30, 50));
+
+// annonymous function
+
+// let func = function (a, b) {
+//     return a + b
+// }
+// console.log(func(20, 40) + func(30 , 50));
+
+// arrow function
+
+// let func = (a, b) => {
+//     return a + b
+// }
+// console.log(func(20 , 40) + func(30 , 50))
+
+// let func = (a , b) => a + b
+// console.log(func(20, 40))
+
+
+// iife
+
+// console.log(((a , b) => {
+//     return a + b
+// })(20, 30));
+
+// console.log(((a, b) => a + b) (20, 30));
+
+// call function
+
+// let sum = (a, b) => a + b
+// let sub = (a, b) => a - b
+// let mul = (a, b) => a * b
+// let div = (a, b) => a / b
+// let rem = (a , b) => a % b
+
+// let all = (a, b, c, d, e) =>  a(20, 30) + b(20, 30) + c(20, 30) + d(20, 30) + e(20, 30)
+
+// console.log(
+//     all(sum, sub, mul, div, rem)
+// );
+
+// let a = 50
+// operator
+// uneri oper
+
+// let a = 30
+// console.log(++a);
+// console.log(a);
+
+// binary operator
+// a = 30
+// b = 50
+
+// console.log(a + b);
+
+// Arthematic operator
+// Assignment operator
+// Comparision operator
+// Logical operator
+// String opertor
+// Conditional operator
+
+
+// Arthematic operator +-*%/
+// post incriment dic
+// pre incriment dic
+
+// a = 30
+// console.log(a++);
+// console.log(a++);
+// console.log(a);
+
+// console.log(--a);
+// console.log(--a);
+// console.log(a);
+
+// console.log(a--);
+// console.log(a--);
+// console.log(a);
+
+
+// a = 10
+// console.log(a++);
+// console.log(--a);
+// console.log(a++);
+// console.log(--a);
+
+// a = 10
+// console.log(--a);
+// console.log(a++);
+// console.log(++a);
+// console.log(--a);
+// console.log(a--);
+// console.log(a);
+
+// Exponential ope.
+
+// console.log(2 ** 2)
+// console.log(3 ** 3)
+// console.log(3 ** 5);
+
+// Assignment operator
+
+// let a = 50
+// b = 40
+// b += a
+// console.log(b);
+
+// let a = 50
+// b = 40
+// b *= a
+// console.log(b);
+
+// let a = 50
+// b = 40
+// b -= a
+// console.log(b);
+
+// let a = 50
+// b = 40
+// b /= a
+// console.log(b);
+
+
+// Comparision operator (<,>,<=,>=,==,===)
+
+// console.log(20 < 30);
+// console.log(30 > 40);
+// console.log(100 < 30);
+// console.log(100 <= 100);
+// console.log(100 == "100");
+// console.log(100 === "100");
+
+
+// Logical operator
+
+// and all condition true
+// or one condition true
+// not opposite kre ga 
+// a = true
+// b = 50
+// c = 30
+// console.log(a == c || b < c);
+// console.log(a == c && b < c);
+// console.log(a);
+
+
+// conditional operator
+
+// age = 40
+// age > 18? console.log("you can vote") : console.log("you canot vote");
+
+// a = false
+// !a ? console.log("you can vote") : console.log("you canot vote");
+
+// a = 100
+// !a ? console.log("you can vote") : console.log("you canot vote");
+
+// a = 0
+// a ? console.log("you can vote") : console.log("you canot vote");
+
+// string operator
+
+// let name = "vijai"
+// age = 60
+// city = "jaipur"
+// console.log("my name is" + name + "and i am" + age + "years old");
